0e175a23a1845f31875b5b5980c27a9adec98c44
prasad291024 <prasadvaliv1992@gmail.com>
2025-01-17 13:49:45 +0530

    17th Jan

---
+++ src/Personal_Practice/BigFactorial.java
@@ -0,0 +1,22 @@
+package Personal_Practice;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigFactorial {
+    public static void main(String args[])
+    {
+        int n, c;
+        BigInteger inc = new BigInteger("1");
+        BigInteger fact = new BigInteger("1");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input an integer");
+        n = input.nextInt();
+        for (c = 1; c <= n; c++) {
+            fact = fact.multiply(inc);
+            inc = inc.add(BigInteger.ONE);
+        }
+        System.out.println(n + "! = " + fact);
+    }
+}
+
