c7b774299f8dd0c7cb7d3d5c0c5b01131e02a1e9
prasad291024 <prasadvaliv1992@gmail.com>
2025-01-20 12:28:33 +0530

    18th Jan Assignment Q5 answer

---
+++ src/ex_04_Ternary_Operator/Q5_OperatorsExample.java
@@ -0,0 +1,27 @@
+package ex_04_Ternary_Operator;
+
+public class Q5_OperatorsExample {
+        public static void main(String[] args) {
+            // Arithmetic Operators
+            int a = 10;
+            int b = 5;
+            System.out.println("Sum: " + (a + b));
+            System.out.println("Difference: " + (a - b));
+
+            // Relational Operators
+            System.out.println("Is a > b? " + (a > b));
+
+            // Logical Operators
+            boolean result = (a > b) && (b > 0);
+            System.out.println("Logical AND result: " + result);
+
+            // Assignment Operators
+            a += 5;
+            System.out.println("New value of a: " + a);
+
+            // Increment/Decrement Operators
+            a++;
+            System.out.println("Incremented value of a: " + a);
+        }
+    }
+
