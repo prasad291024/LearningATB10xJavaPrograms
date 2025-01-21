9c2a17eda90068ddbe041b326bb2d9692a3362d0
prasad291024 <prasadvaliv1992@gmail.com>
2025-01-21 12:43:42 +0530

    Task 19th Jan class program solution

--- src/ex_06_Ternary_Operator/Task19thJan_GradeCalculator.java
+++ src/ex_06_Ternary_Operator/Task19thJan_GradeCalculator.java
@@ -1,6 +1,6 @@
 package ex_06_Ternary_Operator;
 
-public class Task19thJan_ProgramSolution {
+public class Task19thJan_GradeCalculator {
     public static void main(String[] args) {
         int score = 85;
         char grade = (score >= 90) ? 'A' : (score >= 80) ? 'B' : (score >= 70) ? 'C' : (score >= 60) ? 'D' : 'F';
