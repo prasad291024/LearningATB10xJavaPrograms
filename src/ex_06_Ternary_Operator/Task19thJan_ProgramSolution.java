74e23ed2e630c638e5d3feb5f168cd8306562fcd
prasad291024 <prasadvaliv1992@gmail.com>
2025-01-21 12:30:11 +0530

    Task 19th Jan class program solution

---
+++ src/ex_06_Ternary_Operator/Task19thJan_ProgramSolution.java
@@ -0,0 +1,10 @@
+package ex_06_Ternary_Operator;
+
+public class Task19thJan_ProgramSolution {
+    public static void main(String[] args) {
+        int score = 85;
+        char grade = (score >= 90) ? 'A' : (score >= 80) ? 'B' : (score >= 70) ? 'C' : (score >= 60) ? 'D' : 'F';
+
+        System.out.println("The grade for a score of " + score + " is: " + grade);
+    }
+}
