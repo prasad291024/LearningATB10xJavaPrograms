683808ba9ce89f9d465cf31850f7ca5bfd11b2a6
prasad291024 <prasadvaliv1992@gmail.com>
2025-01-29 12:37:26 +0530

    26th Jan

--- src/ex_14_Strings/LabP1_Strings1.java
+++ src/ex_14_Strings/LabP1_Strings1.java
@@ -1,4 +1,10 @@
 package ex_14_Strings;
 
 public class LabP1_Strings1 {
+    public static void main(String[] args) {
+        String s1 = "Prasad"; // SCP
+        System.out.println(s1);
+        s1 = "Valiv";
+        System.out.println(s1);
+    }
 }
--- src/ex_14_Strings/LabP3_Strings_new_keyword.java
+++ src/ex_14_Strings/LabP3_Strings_new_keyword.java
@@ -1,9 +1,9 @@
 package ex_14_Strings;
 
-public class LabP2_Strings2 {
+public class LabP3_Strings_new_keyword {
     public static void main(String[] args) {
         String s1 = "Hello";
-        s1 = s1.concat("World");
-        System.out.println(s1);
+        String s2 = new String("Hello");
     }
 }
+
