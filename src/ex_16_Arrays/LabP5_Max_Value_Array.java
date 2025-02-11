dff917ef5f70d600a53070bef4873a478d2f9df0
prasad291024 <prasadvaliv1992@gmail.com>
2025-02-11 17:40:37 +0530

    11th Feb

--- src/ex_16_Arrays/LabP5_Max_Value_Array.java
+++ src/ex_16_Arrays/LabP5_Max_Value_Array.java
@@ -4,34 +4,37 @@ public class LabP5_Max_Value_Array {
     public static void main(String[] args) {
 
         // my method -
-        int arr[] = {32, 56, 65, 22, 644, 77};
-
-        // find maximum value
-        int max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        System.out.println("Maximum value in the array is: " + max);
-
-        // find minimum value
-        int min = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        System.out.println("Minimum value in the array is: " + min);
-        System.out.println("------------------------------");
+//        System.out.println("My Method");
+//        int arr[] = {32, 56, 65, 22, 644, 77};
+//
+//        // find maximum value
+//        int max = arr[0];
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > max) {
+//                max = arr[i];
+//            }
+//        }
+//        System.out.println("Maximum value in the array is: " + max);
+//
+//        // find minimum value
+//        int min = arr[0];
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] < min) {
+//                min = arr[i];
+//            }
+//        }
+//        System.out.println("Minimum value in the array is: " + min);
+//        System.out.println("------------------------------");
 
         // class taught
-        int array[] = {25, 14, 56, 15, 36, 77, 18, 29, 49};
+        System.out.println("Sir Method");
+        int[] array = {25, 14, 56, 15, 36, 77, 18, 29, 49};
         // MAX, MIN
 
         // Logic Building
         // 1 ->  inputs - array -> int
         // output ->  int - 77
+        //Create a function -
         int max_output = give_max(array);
         int min_output = give_min(array);
         System.out.println(max_output);
