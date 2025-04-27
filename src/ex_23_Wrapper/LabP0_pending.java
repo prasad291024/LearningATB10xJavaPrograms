cc5bb41796563cbe7a5f1ded4865993ba7bfd517
prasad291024 <prasadvaliv1992@gmail.com>
2025-04-27 11:30:49 +0530

    27th April

---
+++ src/ex_23_Wrapper/LabP0_pending.java
@@ -0,0 +1,6 @@
+package ex_23_Wrapper;
+
+public class LabP0_pending {
+    // https://courses.thetestingacademy.com/courses/10x-live-job-ready-automation-tester-blueprint-with-java-by-pramod-dutta-10x/contents/67b19e596c83b
+    // timestamp - 2:18
+}
--- src/ex_23_Wrapper/LabP1.java
+++ src/ex_23_Wrapper/LabP1.java
@@ -1,6 +1,19 @@
 package ex_23_Wrapper;
 
 public class LabP1 {
-    // https://courses.thetestingacademy.com/courses/10x-live-job-ready-automation-tester-blueprint-with-java-by-pramod-dutta-10x/contents/67b19e596c83b
-    // timestamp - 2:18
+    public static void main(String[] args) {
+        int a = 10; // primitive
+
+        // char, short, byte, logn, float, double, - we will avoid them now
+
+
+        // We will use the Wrapper classes,
+        // Character, Boolean, Short, Long, Double, Float
+        Integer a1 = 10;
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(a1.intValue());
+    }
 }
+
