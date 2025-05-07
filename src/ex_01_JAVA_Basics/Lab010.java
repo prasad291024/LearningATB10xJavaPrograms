b3366409bfce4d29d6df01e99ebc9c2e46383bbd
prasad291024 <prasadvaliv1992@gmail.com>
2025-05-07 17:41:11 +0530

    7th May

---
+++ src/ex_01_JAVA_Basics/Lab010.java
@@ -0,0 +1,25 @@
+package ex_01_JAVA_Basics;
+
+public class Lab010 {
+    public static void main(String[] args) {
+        byte b = 10;
+        byte c = 127;
+        byte d = -128;
+        short s = -32768;
+        short s1 = 32767;
+        int i = 2147483647;
+        int i1 = -2147483648;
+        long phone_number = 9876543210l;
+        float f = 12.34f;
+        float f1 = (float) 12.34;
+        double d1 = 12.3456789;
+        char c1 = 'A';
+        char c2 = 'B';
+        char c3 = '$';
+        char c4 = '\n';
+        char c5 = '\u1F60';
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b);
+    }
+}
