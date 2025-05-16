fe526987f06c1886ab930f779d2fdbab09ac0693
prasad291024 <prasadvaliv1992@gmail.com>
2025-05-16 16:11:32 +0530

    16th May

--- src/ex_08_If_Condition/LabP1_If.java
+++ src/ex_08_If_Condition/LabP1_If.java
@@ -2,12 +2,15 @@ package ex_08_If_Condition;
 
 public class LabP1_If {
     public static void main(String[] args) {
-        int age = 17;
+        int age = 18;
 
         if( age >= 18){
             System.out.println("You are allowed to vote!");
 
         }
+
+
+
 //        else{
 //            System.out.println("You are not allowed to vote!");
 //        }
--- src/ex_08_If_Condition/LabP2_If.java
+++ src/ex_08_If_Condition/LabP2_If.java
@@ -4,8 +4,11 @@ public class LabP2_If {
         public static void main(String[] args) {
             int age = Integer.parseInt(args[0]); // convert string input to integer
 
-            if( age > 18){
+            if( age >= 18){
                 System.out.println("You are allowed to vote!");
+            }
+            else{
+                System.out.println("You are not allowed to vote!");
             }
     }
 }
